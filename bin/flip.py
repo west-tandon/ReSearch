8cf7918a87a8df9848670caf72c88c14a255cffa
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2016-12-19 16:31:55 -0500

    rsutils: flip.py executable script

---
+++ bin/flip.py
@@ -0,0 +1,10 @@
+import rsutils
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Flip the most significant bit in every byte of the file.')
+parser.add_argument('input', nargs='?', type=argparse.FileType('br'), default=sys.stdin)
+parser.add_argument('--output', '-o', nargs='?', type=argparse.FileType('bw'), default=sys.stdout)
+args = parser.parse_args()
+
+rsutils.flip_most_significant_bits(args.input, args.output)
