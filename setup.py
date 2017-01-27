c182a43434fd9536532befff2280a232f27aca9f
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-01-26 19:30:09 -0500

    standalone peek & prune scripts

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from distutils.core import setup, Extension
 
 rsutils = Extension('rsutils', sources=['research/rsutils.c'])
 
-setup (name='ReSearch',
-       version='1.0',
-       description='Research tools for Text Search and Information Retrieval',
-       ext_modules=[rsutils])
+setup(name='ReSearch',
+      version='1.0',
+      description='Research tools for Text Search and Information Retrieval',
+      download_url='https://github.com/west-tandon/ReSearch',
+      license='MIT',
+      packages=['research', 'research.coding', 'research.index'],
+      ext_modules=[rsutils],
+      install_requires=[
+          'argparse',
+          'importlib'
+      ])
