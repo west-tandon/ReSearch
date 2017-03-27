ab8511a7a7374b02a60acfe648dd0fd7147ec101
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-03-27 18:21:47 -0400

    Finding by ID/title

--- bin/findd.py
+++ bin/findd.py
@@ -5,20 +5,28 @@ from research.index.common import IndexFactory
 
 parser = argparse.ArgumentParser(description='Flip the most significant bit in every byte of the file.')
 parser.add_argument('index', type=str, help='JSON file of the input index')
-parser.add_argument('--start', '-s', nargs='?', default=0, type=int, help='the document index to start with')
+g = parser.add_mutually_exclusive_group(required=True)
+g.add_argument('--title', '-t', type=str, help='title to search for')
+g.add_argument('--docid', '-d', type=int, help='document ID to search for')
 
 args = parser.parse_args()
 
 index = IndexFactory.from_path(args.index)
 sys.stderr.write('Loading index...')
+sys.stderr.flush()
 reader = index.reader()
 sys.stderr.write(' Done.\n')
-reader.skip(args.start)
 
-while True:
-    document = reader.next_document()
-    if document is None:
-        break
+document = None
+
+if args.title is not None:
+    document = reader.find_by_title(args.title)
+else:
+    document = reader.find_by_id(args.docid)
+
+if document is None:
+    print("Couldn't find document {}".format(args.title))
+else:
     sys.stdout.write("\n# Document {0}: {1}\n".format(document.doc_id, document.title))
     term = document.next_term()
     if term is not None:
@@ -29,12 +37,3 @@ while True:
             sys.stdout.write(term)
             term = document.next_term()
         sys.stdout.write('\n')
-    cmd = input('')
-    if cmd == 'q':
-        sys.stderr.write('Quitting...\n')
-        break
-    try:
-        s = int(cmd)
-        reader.skip(s)
-    except ValueError:
-        pass
